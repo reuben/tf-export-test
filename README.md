3c2f4a2325c5c30246bad3886630b2e79e8448f5
Reuben Morais <reuben.morais@gmail.com>
2017-02-28 16:27:46 -0300

    Simplify testcase

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ python train.py
 # Freeze graph (embed checkpointed values as constant ops)
 python ../tensorflow/tensorflow/python/tools/freeze_graph.py --input_graph=graph-def/graph.pb --input_binary --input_checkpoint=checkpoints/model.checkpoint --output_graph=graph-def/frozen_graph.pb --output_node_names=y
 # Quantize graph
-python ../tensorflow/tensorflow/tools/quantization/quantize_graph.py --input=graph-def/frozen_graph.pb --output_node_names=y --output=graph-def/quantized_graph.pb --mode=eightbit
+python ../tensorflow/tensorflow/tools/quantization/quantize_graph.py --input=graph-def/frozen_graph.pb --output_node_names=y --output=graph-def/quantized_graph.pb --mode=weights
 # Import quantized graph and test it
 python import.py
 ```
--- import.py
+++ import.py
@@ -1,25 +1,19 @@
+# coding: utf-8
 import tensorflow as tf
-from os import path, getcwd
+from os import path
 
-BASEDIR = getcwd()
-
-input_graphdef = path.join(BASEDIR, "graph-def", "quantized_graph.pb")
+input_graphdef = path.join("graph-def", "quantized_graph.pb")
 
 graph = tf.Graph()
 with graph.as_default():
     pb = tf.GraphDef()
     with open(input_graphdef, "rb") as fin:
-        # pb.MergeFromString(fin.read()) # text
+        # text_format.Merge(fin.read(), pb) # text
         pb.ParseFromString(fin.read()) # binary
     imports = tf.import_graph_def(pb, name="")
     
-    x = graph.get_tensor_by_name("x:0")
-    x_len = graph.get_tensor_by_name("x_len:0")
-    y = graph.get_tensor_by_name("y:0")
-    
+    outputs = graph.get_tensor_by_name("y:0")
+
     sess = tf.Session()
-    pred = sess.run(y, feed_dict={
-        x: [[[2.0,2.1,2.2,2.3,2.4,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.]]],
-        x_len: [5]
-    })
-    print("prediction: {}".format("linear" if pred == 0 else "random"))
+    result = sess.run(outputs)
+    print("result: {}".format(result))
